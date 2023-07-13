5eaca2def5b0eb5ede149c4054225350e3516729
Jacob Homanics <homanicsjake@gmail.com>
2023-07-13 14:20:04 -0500

    added project

---
+++ index.js
@@ -0,0 +1,13 @@
+// import {
+//     hatIdDecimalToHex,
+//     treeIdDecimalToHex,
+//     hatIdHexToDecimal,
+//     treeIdHexToDecimal,
+//     hatIdDecimalToIp,
+//   } from "@hats-protocol/sdk-v1-core";
+
+import { HatsClient } from "@hats-protocol/sdk-v1-core";
+
+//   const { HatsClient } = require("@hats-protocol/sdk-v1-core");
+
+  console.log("aye");
